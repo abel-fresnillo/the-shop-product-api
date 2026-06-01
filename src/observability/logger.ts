347ff84c45b06b65dee7793b76ae0fd85467c76e
Abel Fresnillo <abel@Abels-MacBook-Air.local>
2026-05-31 23:22:04 -0400

    Fixes Winston logging issue

--- src/observability/logger.ts
+++ src/observability/logger.ts
@@ -1,4 +1,5 @@
 import winston from "winston";
+import { OpenTelemetryTransportV3 } from "@opentelemetry/winston-transport";
 
 export const logger = winston.createLogger({
   level: process.env.LOG_LEVEL ?? "info",
@@ -9,5 +10,8 @@ export const logger = winston.createLogger({
           winston.format.simple()
         )
       : winston.format.json(),
-  transports: [new winston.transports.Console()],
+  transports: [
+    new winston.transports.Console(),
+    ...(process.env.NODE_ENV !== "test" ? [new OpenTelemetryTransportV3()] : []),
+  ],
 });
